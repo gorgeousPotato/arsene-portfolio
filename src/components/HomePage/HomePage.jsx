6eb7bd42496ed1a29d66fa0231b8ee4db7760a02
gorgeousPotato <lanauzbekov@gmail.com>
2024-01-30 22:50:02 -0800

    first touches of style

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,10 +3,9 @@ import "./HomePage.css"
 export default function HomePage() {
   return (
     <div className="HomePage">
-      <h1>Hi, my name is Arsene</h1>
-      <h3>PhD in Organic Chemistry, HPA enthusiast</h3>
-      <div className="avatar">
-        <h3>avatar here</h3>
+      <div className="gradient">
+        <h1>Hi, my name is Arsene</h1>
+        <h3>PhD in Organic Chemistry, sustainability enthusiast</h3>
       </div>
     </div>
   );
